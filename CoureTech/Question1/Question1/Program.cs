b09710d01acd9aa252ab21a8191f2eafcf313658
Adebo Timothy <timtyger796@gmail.com>
2022-03-22 22:01:03 +0100

    Add files via upload
    solution to Q1

---
+++ CoureTech/Question1/Question1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace Question1
+{
+    class Program
+    {
+        static void TotalArray(int[] input)
+        {
+            int output = 0;
+            foreach (var item in input)
+            {
+                if (item % 2 == 0)
+                {
+                    output += 1;
+                    if (item == 8)
+                    {
+                        output += 5;
+                    }
+                }
+                else output += 3;
+                
+            }
+            Console.WriteLine(output);
+        }
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Enter numbers seperated by comma (e.g 1,2,3):");
+            string inputParams = Console.ReadLine();
+            var splitInput = inputParams.Split(',');
+            var input = splitInput.Select(int.Parse).ToArray();
+            TotalArray(input);
+            Console.ReadKey();
+        }        
+    }
+}

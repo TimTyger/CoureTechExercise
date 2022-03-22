42663b05f6e88c0e4fe356d74a32025a14d96d3c
Adebo Timothy <timtyger796@gmail.com>
2022-03-22 22:47:48 +0100

    Add files via upload

---
+++ Question2/Question2.Repo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Question2.Repo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Question2/Question2.Services/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Question2.Services
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

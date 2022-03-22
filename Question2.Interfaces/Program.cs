35d4f3597b036fbc77f9368cff060b272e4417d6
adebortimothy <adebortimothy@gmail.com>
2022-03-22 22:57:38 +0100

    Add project files.

---
+++ Question2.Interfaces/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Question2.Interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
